a7083fcbfc7fa1149a2c0691daf0b8375530c8e2
Rob Gafken <rtd91@live.com>
2019-10-04 19:04:00 -0400

    Add Functionality to get all products

---
+++ GroceryStoreAPI/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using GroceryStoreAPI.Interfaces;
+using GroceryStoreAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroceryStoreAPI.Controllers
+{
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        public IGroceryStoreDbContext GroceryStoreDbContext { get; }
+
+        public ProductController(IGroceryStoreDbContext groceryStoreDbContext)
+        {
+            GroceryStoreDbContext = groceryStoreDbContext;
+        }
+
+        [Route("products")]
+        public Product[] GetProducts()
+        {
+            return GroceryStoreDbContext.Products.ToArray();
+        }
+    }
+}
---
+++ GroceryStoreAPITests/Controllers/ProductControllerShould.cs
@@ -0,0 +1,32 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using GroceryStoreAPI.Controllers;
+using GroceryStoreAPI.Interfaces;
+using GroceryStoreAPI.Models;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace GroceryStoreAPITests.Controllers
+{
+    public class ProductControllerShould
+    {
+        private Fixture _fixture = new Fixture();
+
+        [Fact]
+        public void GetAllProductsWhenGetProductsIsCalled()
+        {
+            var expected = _fixture.CreateMany<Product>(5).ToList();
+            var context = Substitute.For<IGroceryStoreDbContext>();
+            context.Products.Returns(expected);
+
+            var controller = new ProductController(context);
+            var actual = controller.GetProducts();
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}
