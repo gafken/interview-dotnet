e76948a62f53e0cac35684c631b5962a159458c0
Rob Gafken <rtd91@live.com>
2019-10-04 18:17:37 -0400

    Add OrderDate To Orders

--- GroceryStoreAPI/Models/Order.cs
+++ GroceryStoreAPI/Models/Order.cs
@@ -1,11 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace GroceryStoreAPI.Models
 {
     public class Order
     {
         public int Id { get; set; }
-        public int CusomerId { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime OrderDate { get; set; }
         public List<Item> Items { get; set; }
     }
 }
--- GroceryStoreAPITests/GroceryStoreDbContextShould.cs
+++ GroceryStoreAPITests/GroceryStoreDbContextShould.cs
@@ -1,5 +1,9 @@
 using FluentAssertions;
 using GroceryStoreAPI;
+using GroceryStoreAPI.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace GroceryStoreAPITests
@@ -10,9 +14,69 @@ namespace GroceryStoreAPITests
         public void SetPropertiesOnInitialization()
         {
             var context = new GroceryStoreDbContext();
-            context.Customers.Count.Should().Be(3);
-            context.Orders.Count.Should().Be(1);
-            context.Products.Count.Should().Be(3);
+            context.Customers.Should().BeEquivalentTo(new List<Customer>
+            {
+                new Customer
+                {
+                    Id = 1,
+                    Name = "Bob"
+                },
+                new Customer
+                {
+                    Id = 2,
+                    Name = "Mary"
+                },
+                new Customer
+                {
+                    Id = 3,
+                    Name = "Joe"
+                }
+            });
+
+            context.Products.Should().BeEquivalentTo(new List<Product>
+            {
+                new Product
+                {
+                    Id = 1,
+                    Description = "apple",
+                    Price = 0.5m
+                },
+                new Product
+                {
+                    Id = 2,
+                    Description = "orange",
+                    Price = 0.75m
+                },
+                new Product
+                {
+                    Id = 3,
+                    Description = "banana",
+                    Price = 0.85m
+                }
+            });
+
+            context.Orders.Should().BeEquivalentTo(new List<Order>
+            {
+                new Order
+                {
+                    Id = 1,
+                    CustomerId = 1,
+                    OrderDate = DateTime.Parse("2012-04-23T18:25:43.511"),
+                    Items = new List<Item>
+                    {
+                        new Item
+                        {
+                            ProductId = 1,
+                            Quantity = 2
+                        },
+                        new Item
+                        {
+                            ProductId = 2,
+                            Quantity = 3
+                        }
+                    }
+                }
+            });
         }
     }
 }
