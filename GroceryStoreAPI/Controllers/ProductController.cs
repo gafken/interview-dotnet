f961afbaedf1fe0bd6b30421cacfa7cc3f72f290
Rob Gafken <rtd91@live.com>
2019-10-04 19:09:53 -0400

    Add Functionality for get Products by Id

--- GroceryStoreAPI/Controllers/ProductController.cs
+++ GroceryStoreAPI/Controllers/ProductController.cs
@@ -24,9 +24,10 @@ namespace GroceryStoreAPI.Controllers
             return GroceryStoreDbContext.Products.ToArray();
         }
 
+        [Route("product/{id}")]
         public object GetProductById(int id)
         {
-            throw new NotImplementedException();
+            return GroceryStoreDbContext.Products.SingleOrDefault(x => x.Id == id);
         }
     }
 }
